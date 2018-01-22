c1683fa2b80ef904d95bbdad8f42776b22c3c8c1
Rungrot Suptan <rungrot.sup@gmail.com>
2018-01-22 13:13:12 +0700

    installing note

---
+++ Installing.js
@@ -0,0 +1,20 @@
+Installing
+
+- Express
+
+sudo npm install express --save
+
+sudo npm install express-generator -g
+
+express --view=ejs myapp
+
+cd myapp 
+
+npm install 
+
+npm start //Start server
+
+- Nodemon //auto restart there server
+
+npm install nodemon //installing
+npx nodemon //command to running use only one time
