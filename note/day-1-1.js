b20ac0e44d67b961e379850a8804321e8e3b0a86
Rungrot Suptan <rungrot.sup@gmail.com>
2018-01-22 13:17:50 +0700

    prepare note

---
+++ note/day-1-1.js
@@ -0,0 +1,21 @@
+Installing
+
+- Express
+
+sudo npm install express --save
+
+sudo npm install express-generator -g
+
+express --view=ejs myapp
+
+cd myapp 
+
+npm install 
+
+npm start //Start server
+npm stop //Stop server
+
+- Nodemon //tool for auto restart there server
+
+npm install nodemon //installing
+npx nodemon //command to running use only one time
